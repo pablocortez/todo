3fcb39ad9c1e90c0dcc1b5b990f81df8dff58e6b
Pablo Cortez <pabloecortez07@gmail.com>
2017-01-25 00:11:42 -0800

    fixed Percentage issue

--- resources/js/date.js
+++ resources/js/date.js
@@ -2,18 +2,27 @@
 function getDayProgress() {
 	var date = new Date();
 
-	var minutes_so_far = date.getMinutes() + (date.getHours() - 5) * 60;
-  console.log(minutes_so_far);
+	var minutes_so_far = date.getMinutes() + (date.getHours() * 60);
 
-	var minutes_in_a_day = 1440;
+	var hour = date.getHours();
+
+	var minutes_in_a_day = 19 * 60;
 
 	var day_progress = minutes_so_far / minutes_in_a_day;
-
-  console.log("Day progress: " + day_progress);
 
   var percent = (day_progress * 100).toFixed(2);
 
   document.getElementById('day-progress').innerHTML = percent + '% of day elapsed';
+
+	switch (hour) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			document.getElementById('day-progress').innerHTML = "0" + '% of day elapsed';
+			break;
+	}
 }
 
 getDayProgress();
